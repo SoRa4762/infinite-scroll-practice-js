4853d4c9036d772da710486f5597bd346e953d40
来 SoRa <sonamraisx007@gmail.com>
2023-04-03 15:38:46 +0545

    learning infinite scroll in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from "react";
+import "./App.css";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={query} onChange={handleChange} />
+      {/* {console.log(books)} */}
+      {books.map((book, key) => {
+        // console.log(book);
+        return (
+          <div ref={lastBookElementRef} key={key}>
+            {book}
+          </div>
+        );
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error..."}</div>
+    </>
   );
 }
 
